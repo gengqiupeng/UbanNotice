686c3afebadc1a5efe47faa9038f66491a6adbcc
gengqiupeng <961902202@qq.com>
2020-09-17 08:53:27 +0800

    测试模式下，取消邮件发送

--- src/notice/MailNotice.php
+++ src/notice/MailNotice.php
@@ -5,6 +5,7 @@ namespace uban\notice;
 use Exception;
 use think\Container;
 use think\exception\Handle;
+use think\facade\App;
 use think\facade\Config;
 use think\Response;
 use uban\mail\UbanMail;
@@ -18,30 +19,33 @@ class MailNotice extends Handle
      */
     public function render(Exception $e)
     {
+        if (App::isDebug()) {
+            return parent::render($e);
+        }
         // 请求异常
         if ($e instanceof Exception) {
 
-                // 调试模式，获取详细的错误信息
-                $data = [
-                    'name'    => get_class($e),
-                    'file'    => $e->getFile(),
-                    'line'    => $e->getLine(),
-                    'message' => $this->getMessage($e),
-                    'trace'   => $e->getTrace(),
-                    'code'    => $this->getCode($e),
-                    'source'  => $this->getSourceCode($e),
-                    'datas'   => $this->getExtendData($e),
-                    'tables'  => [
-                        'GET Data'              => $_GET,
-                        'POST Data'             => $_POST,
-                        'Files'                 => $_FILES,
-                        'Cookies'               => $_COOKIE,
-                        'Session'               => isset($_SESSION) ? $_SESSION : [],
-                        'Server/Request Data'   => $_SERVER,
-                        'Environment Variables' => $_ENV,
-                        'ThinkPHP Constants'    => $this->ubanGetConst(),
-                    ],
-                ];
+            // 调试模式，获取详细的错误信息
+            $data = [
+                'name'    => get_class($e),
+                'file'    => $e->getFile(),
+                'line'    => $e->getLine(),
+                'message' => $this->getMessage($e),
+                'trace'   => $e->getTrace(),
+                'code'    => $this->getCode($e),
+                'source'  => $this->getSourceCode($e),
+                'datas'   => $this->getExtendData($e),
+                'tables'  => [
+                    'GET Data'              => $_GET,
+                    'POST Data'             => $_POST,
+                    'Files'                 => $_FILES,
+                    'Cookies'               => $_COOKIE,
+                    'Session'               => isset($_SESSION) ? $_SESSION : [],
+                    'Server/Request Data'   => $_SERVER,
+                    'Environment Variables' => $_ENV,
+                    'ThinkPHP Constants'    => $this->ubanGetConst(),
+                ],
+            ];
 
             //保留一层
             while (ob_get_level() > 1) {
@@ -55,7 +59,7 @@ class MailNotice extends Handle
             include "exception.html";
 
             // 获取并清空缓存
-            $content  = ob_get_clean();
+            $content = ob_get_clean();
             $userConfig = new UbanMailUserConfig();
             $config = $userConfig->getMailConfig();
             $sendTo = Config::get("app.bug_mail_to");
@@ -68,6 +72,7 @@ class MailNotice extends Handle
         // 其他错误交给系统处理
         return parent::render($e);
     }
+
     /**
      * 获取常量列表
      * @return array 常量列表
